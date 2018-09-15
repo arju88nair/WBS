ed02f2fc1fce4397eadcc06c7da28cb763fabd37
arju88nair <arju88nair@gmail.com>
2018-09-15 21:42:35 +0530

    intial commit with sqlalchemy connection

---
+++ bookmanager.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file= 'mysql://root:@127.0.0.1/wds'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+
+db = SQLAlchemy(app)
+
+class BluePrint(db.Model):
+    name = db.Column(db.Integer, primary_key=True)
+
+   
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+   return "DFDFDF"
+
+if __name__ == "__main__":
+    app.run(debug=True)
