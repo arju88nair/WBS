8d2f7d3046e4f1d22fd7a5ef23cf0fc3cae1de9f
arju88nair <arju88nair@gmail.com>
2018-09-17 21:37:47 +0530

    base architecture and pplan view

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask import request
+import urllib
+from flask import send_from_directory
+from sqlalchemy import or_
+
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -27,11 +31,17 @@ class BluePrint(db.Model):
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    pid = request.args.get("pid")
     blueprints = BluePrint.query.all()
+    if pid :
+        blueprints = BluePrint.query.filter(or_(BluePrint.parent_id == pid,BluePrint.id == pid))
+
     return render_template("home.html",blueprints=blueprints)
 
+
+
 @app.route('/downloadCsv', methods=["GET", "POST"])
-def getCsv():    
+def index():    
     records = BluePrint.query.all()
     with open('vehicle.csv', 'w') as f_handle:
         writer = csv.writer(f_handle)
@@ -40,6 +50,13 @@ def getCsv():
         writer.writerow(header)
         # Iterate over `data`  and  write to the csv file
         [writer.writerow([getattr(curr, column.name) for column in BluePrint.__mapper__.columns]) for curr in records]
+    # return redirect("vehicle.csv", code=302)
+    return send_from_directory('',
+                               'vehicle.csv', as_attachment=True)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
